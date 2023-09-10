18104e54078ae5ddb1086e2b4b04fed96d0f99dc
ilnickii <ilnickii2008@gmail.com>
2023-09-10 14:58:37 +0300

    Add task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+form.addEventListener('input', throttle(function() {
+  const feedbackState = { email: emailInput.value, message: messageInput.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
+}, 500));
+
+const storedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (storedState) {
+  emailInput.value = storedState.email;
+  messageInput.value = storedState.message;
+}
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+
+  const feedbackData = { email: emailInput.value, message: messageInput.value };
+  console.log(feedbackData);
+});
+
+
 
